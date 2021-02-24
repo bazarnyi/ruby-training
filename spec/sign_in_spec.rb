2df4c9b4d2d847630595c7c1beae5aa271592d4a
Denys Bazarnyi <den.bazarny@gmail.com>
2021-02-24 11:35:26 +0200

    Initial test created

---
+++ spec/sign_in_spec.rb
@@ -0,0 +1,14 @@
+feature 'Sign in user', js: true do
+  scenario 'User can log in' do
+    visit('http://testautomate.me/redmine/')
+    expect(page).to have_content 'Redmine@testautomate.me'
+
+    find('.login').click
+
+    find('#username').set 'test'
+    find('#password').set 'test1234'
+    find('#login-submit').click
+
+    expect(page).to have_content 'Logged in as test'
+  end
+end
