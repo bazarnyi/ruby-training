c03c1e8e2677cecf271a11754eda7b73f02097cc
Denys Bazarnyi <den.bazarny@gmail.com>
2021-03-17 11:26:53 +0200

    User model added

---
+++ models/user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'ffaker'
+
+# This class describes User Model
+class User
+  attr_reader :username, :password, :firstname, :lastname, :email
+
+  def initialize
+    @username = FFaker::Random.seed.to_s
+    @password = FFaker::Internet.password
+    @firstname = FFaker::Name.first_name
+    @lastname = FFaker::Name.last_name
+    @email = FFaker::Internet.email
+  end
+end
--- spec/sign_up_spec.rb
+++ spec/sign_up_spec.rb
@@ -9,18 +9,8 @@ feature 'Sign up user', js: true do
 
     @home_page.menu.sign_up_link.click
 
-    @sign_up_page = SignUpPage.new
-    random = SecureRandom.hex
+    sign_up_user
 
-    @sign_up_page.login.set "test#{random}"
-    @sign_up_page.password.set 'test1234'
-    @sign_up_page.password_confirm.set 'test1234'
-    @sign_up_page.firstname.set 'Test'
-    @sign_up_page.lastname.set 'User'
-    @sign_up_page.email.set "test#{random}@test.org"
-
-    @sign_up_page.submit_btn.click
-
-    expect(@home_page.menu.logged_as.text).to include 'Logged in as test'
+    expect(@home_page.menu.logged_as.text).to include "Logged in as #{@user.username}"
   end
 end
